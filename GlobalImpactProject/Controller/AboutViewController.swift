42ff555323554d8000ea2ea659f2667d6801b777
Kennedy <nken8869@740s-pl-103.local>
2019-09-26 13:17:59 -0600

    Added a view controller GUI Component

--- GlobalImpactProject/Controller/AboutViewController.swift
+++ GlobalImpactProject/Controller/AboutViewController.swift
@@ -2,20 +2,19 @@
 //  ViewController.swift
 //  GlobalImpactProject
 //
-//  Created by Kennedy, Niklas on 9/26/19.
+//  Created by Kennedy, Niklas on 9/20/19.
 //  Copyright © 2019 CTEC. All rights reserved.
 //
 
-import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+class AboutViewController: UIViewController {
 
     override func viewDidLoad() -> Void {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading th, typically from a nib.
     }
-}
 
+
+}
 
